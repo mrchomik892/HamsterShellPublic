e46c938f2688b2e9fb3ec542ac9e456e2296d791
mrchomik892 <54484477+mrchomik892@users.noreply.github.com>
2019-10-01 09:14:23 +0200

    Files of the HamsterShell
    HamsterShell files without an Uninstall.exe, because it don't know where is Hamster Shell

---
+++ HamsterShell.cmd
@@ -0,0 +1,13 @@
+echo off
+cls
+echo off
+cls
+color 4
+echo HamsterShell(C) by Chomiczeq20
+echo HamsterShell is on License...
+echo MIT License
+echo Do you readed the License?
+echo Disagree = Close program
+echo Agree = Any key
+timeout /t 50
+hamster.bat
---
+++ hamster.bat
@@ -0,0 +1,6 @@
+color 1F
+echo Welcome to HamsterShell!
+:dupa
+set /p cmd=HS %cd% 
+%cmd%
+GOTO dupa
