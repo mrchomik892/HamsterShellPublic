00b1421787397e5e4168d0ba87cd29a2f7b6ad88
zdziwionyrysiczeq <54484477+mrchomik892@users.noreply.github.com>
2019-11-28 13:21:17 +0100

    Update HamsterShell.cmd

--- HamsterShell.cmd
+++ HamsterShell.cmd
@@ -3,14 +3,13 @@ cls
 echo off
 cls
 color 4
-echo HamsterShell(C) by Chomiczeq20
+echo HamsterShell(C) v0.2 by Chomiczeq20
 echo HamsterShell is on License...
 echo GPLv3
 echo Do you readed the License?
 echo Disagree = Close program
 echo Agree = Any key
 pause
-hamster.bat
 color 1F
 echo Welcome to HamsterShell!
 :acocieto
